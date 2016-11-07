c4cfbe486a1f286057e62e4f7609214b10ab29e8
Travis Lilleberg <travis.lilleberg@tufts.edu>
2016-11-07 11:36:55 -0500

    Added tests for get_stream

--- spec/lib/fedora_helpers_spec.rb
+++ spec/lib/fedora_helpers_spec.rb
@@ -4,18 +4,22 @@ shared_examples_for 'FedoraHelpers' do
   let(:pid) { 'tufts:MS054.003.DO.02108' }
 
   describe "find" do
-    it "saves a valid ID as an ActiveFedora object in @fedora_object" do
-      valid = described_class.new("blah")
-      valid.find(pid)
-      expect(
-        valid.instance_variable_get(:@fedora_object).class
-      ).to eq(ActiveFedora::Base)
+    context "with a valid ID" do
+      it "saves an ActiveFedora object in @fedora_object" do
+        valid = described_class.new("blah")
+        valid.find(pid)
+        expect(
+          valid.instance_variable_get(:@fedora_object).class
+        ).to eq(ActiveFedora::Base)
+      end
     end
 
-    it "saves an invalid ID as an empty hash in @fedora_object" do
-      invalid = described_class.new("blah")
-      invalid.find("blah")
-      expect(invalid.instance_variable_get(:@fedora_object)).to be_empty
+    context "with an invalid ID" do
+      it "saves an empty hash in @fedora_object" do
+        invalid = described_class.new("blah")
+        invalid.find("blah")
+        expect(invalid.instance_variable_get(:@fedora_object)).to be_empty
+      end
     end
   end
 
@@ -32,15 +36,26 @@ shared_examples_for 'FedoraHelpers' do
         expect(obj.get_stream(ds).class).to eq(FedoraHelpers::Datastream)
       end
 
-      it "saves stream to @streams if not already there"
-      it "doesn't save stream to @streams if already there"
+      it "saves new stream to @streams" do
+        obj.get_stream(ds)
+        expect(obj.instance_variable_get(:@streams)).to have_key(ds.to_sym)
+      end
+
+      it "doesn't save to @streams if already there" do
+        obj.instance_variable_set(:@streams, { ds.to_sym => "bad value" } )
+        expect(obj.get_stream(ds)).to eq("bad value")
+      end
     end
 
     context "with an invalid datastream" do
       it "returns nil" do
         expect(obj.get_stream("alskdjfa;sk")).to be_nil
       end
-      it "saves nothing to @streams"
+
+      it "saves nothing to @streams" do
+        obj.get_stream("adsfasdf")
+        expect(obj.instance_variable_get(:@streams)).to be_empty
+      end
     end
   end
 
