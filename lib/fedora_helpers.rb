b13d33418b4bfe2216fb9797457d9e56e3884293
Travis Lilleberg <travis.lilleberg@tufts.edu>
2016-11-08 11:45:58 -0500

    Finished(?) FedoraHelpers, FedoraHelpers::Datastream and all tests for those.

--- lib/fedora_helpers.rb
+++ lib/fedora_helpers.rb
@@ -1,4 +1,3 @@
-##
 ## @file
 # Helper methods for working with Fedora, ActiveFedora, and Nokogiri
 
@@ -11,12 +10,11 @@ module FedoraHelpers
 
 
   ##
-  # Retrieves an ActiveFedora object, or {} if the pid's invalid.
+  # Saves a Fedora record into @fedora_object.
+  # Saves an empty hash if record not found.
   #
   # @params
   #   id {string} Fedora pid.
-  # @return {object/hash}
-  #   ActiveFedora object or empty hash.
   def find(id)
     if(ActiveFedora::Base.exists?(id))
       @fedora_object = ActiveFedora::Base.find(id)
@@ -33,7 +31,7 @@ module FedoraHelpers
   #
   # @params
   #   name {string} Datastream name.
-  # @return {object/hash}
+  # @return {FedoraHelpers::Datastream / hash}
   #   FedoraHelpers::Datastream object or empty hash.
   def get_stream(name)
     if(@streams.nil?)
@@ -43,7 +41,8 @@ module FedoraHelpers
     if(@fedora_object.datastreams.key?(name))
       nym = name.to_sym
       unless(@streams.key?(nym))
-        @streams[nym] = FedoraHelpers::Datastream.new(@fedora_object.datastreams[name])
+        @streams[nym] =
+          FedoraHelpers::Datastream.new(@fedora_object.datastreams[name])
       end
       @streams[nym]
     else
@@ -56,16 +55,108 @@ module FedoraHelpers
   ##
   # Datastream-specific helpers.
   class Datastream
+    # Our Nokogiri::XML document.
     attr_reader :xml
+    # Our default root element to work from.
+    attr_accessor :default_root
 
+    ##
+    # Loads the xml into @xml.
+    #
+    # @params
+    #   stream {ActiveFedora::Datastream} Datastream to parse.
     def initialize(stream)
       unless(stream.respond_to?(:content))
         @xml = ""
         Rails.logger.warn("Error, #{stream} is not a datastream.")
       else
         @xml = Nokogiri::XML(stream.content.to_s)
+        @default_root = "/#{get_root}"
       end
     end
 
+    ##
+    # Gets the root node name and namespace.
+    #
+    # @return {string}
+    #   Root node name, with namespace.
+    def get_root
+      get_full_node_name(@xml.root)
+    end
+
+    ##
+    # Gets the text from the first element that matches xpath.
+    #
+    # @params
+    #   path {string} Element name or path to search for.
+    # @return {string}
+    #   The text from the element or "".
+    def get_text(path)
+      elements = xpath(path)
+      if(elements.empty?)
+        ""
+      else
+        elements.first.text
+      end
+    end
+
+    ##
+    # Gets an array of all the texts from elements that match xpath.
+    #
+    # @params
+    #   path {string} Element name or path to search for.
+    # @return {array}
+    #   An array of the text values of all the matching elements.
+    def get_all_text(path)
+      values = []
+      elements = xpath(path)
+      elements.each do |el|
+        values.push(el.text)
+      end
+      values
+    end
+
+
+    private
+
+    ##
+    # Performs an xpath query.
+    #
+    # Paths that start with '/' will be taken verbatim.
+    # Paths that don't will be concat'd to @default_root.
+    #
+    # @params
+    #   path {string} Xpath string to search for.
+    # @return {array}
+    #   Set of elements that match the query.
+    def xpath(path)
+      unless(path[0] == "/")
+        path = "#{@default_root}/#{path}"
+      end
+
+      begin
+        @xml.xpath(path)
+      rescue Nokogiri::XML::XPath::SyntaxError
+        Rails.logger.warn("Syntax error searching for #{path}.")
+        []
+      end
+    end
+
+    ##
+    # Gets the node name prefixed by its ns, if it has one.
+    #
+    # @params
+    #   node {Nokogiri::XML::Element} Target element.
+    # @return {string}
+    #   Node name, with namespace if applicable.
+    def get_full_node_name(node)
+      if(node.namespace.prefix.to_s == "")
+        element.name
+      else
+        "#{node.namespace.prefix}:#{node.name}"
+      end
+    end
   end # End Datastream
+
 end # End FedoraHelpers
+
--- spec/lib/fedora_helpers_datastream_spec.rb
+++ spec/lib/fedora_helpers_datastream_spec.rb
@@ -3,22 +3,64 @@ require 'rails_helper'
 describe FedoraHelpers::Datastream do
   let(:pid) { "tufts:MS054.003.DO.02108" }
   let(:ds_name) { "DCA-META" }
-  let(:stream) { ActiveFedora::Base.find(pid).datastreams[ds_name] }
+  let(:obj) {
+    stream = ActiveFedora::Base.find(pid).datastreams[ds_name]
+    FedoraHelpers::Datastream.new(stream)
+  }
 
   describe "initialization" do
     context "with a valid stream" do
-      it "saves a Nokogiri document to @xml" do
-        obj = FedoraHelpers::Datastream.new(stream)
-        expect(obj.instance_variable_get(:@xml).class).to eq(Nokogiri::XML::Document)
+      it "saves a Nokogiri document to attr_readable @xml" do
+        expect(obj.xml.class).to eq(Nokogiri::XML::Document)
+      end
+
+      it "sets default_root to be the root element" do
+        expect(obj.default_root).to eq("/#{obj.get_root}")
       end
     end
 
     context "with an invalid stream" do
-      it "saves an empty string to @xml" do
+      it "saves an empty string to xml" do
         obj = FedoraHelpers::Datastream.new({})
-        expect(obj.instance_variable_get(:@xml)).to eq("")
+        expect(obj.xml).to eq("")
       end
     end
   end # End describe initialization
 
-end # End describe FedoraHelpers::Datastream
+  describe "get_root" do
+    it "returns the root element's name" do
+      expect(obj.get_root).to eq("dca_dc:dc")
+    end
+  end
+
+  describe "get_text" do
+    context "with valid path" do
+      it "returns the text from the first matching element" do
+        expect(obj.get_text("dcadesc:subject")).to eq("Houses")
+      end
+    end
+
+    context "with invalid path" do
+      it "returns an empty string" do
+        expect(obj.get_text("dc:liajd")).to eq("")
+      end
+    end
+
+    # No testing for syntax errors here. That's done in get_all_text.
+  end
+
+  describe "get_all_text" do
+    it "returns an array of all texts from matching elements" do
+      correct_answer = ["Houses", "Edwin B. Rollins, papers"]
+      expect(obj.get_all_text("dcadesc:subject")).to eq(correct_answer)
+    end
+
+    context "with syntax error" do
+      it "swallows the exception and still returns an empty array" do
+        expect(obj.get_all_text("bad_ns:blah")).to eq([])
+      end
+    end
+  end # End describe get_all_text
+
+end
+
