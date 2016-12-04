6bb0340645bd9299b841ad739c1ff5248bb163b8
Travis Lilleberg <travis.lilleberg@tufts.edu>
2016-12-04 11:40:03 -0500

    Getting rid of those annoying paper trail notices.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Get rid of that annoying paper trail error
+  def user_for_paper_trail
+    nil
+  end
 end
