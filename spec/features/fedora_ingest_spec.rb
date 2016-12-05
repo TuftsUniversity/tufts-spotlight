12809d4eebaf36e8f1191ca4ab5576df0b93b572
Travis Lilleberg <travis.lilleberg@tufts.edu>
2016-12-05 11:49:17 -0500

    Added a explanation of factories to spec/factories and the feature test is working.

--- spec/features/fedora_ingest_spec.rb
+++ spec/features/fedora_ingest_spec.rb
@@ -7,7 +7,16 @@ feature "Fedora 3 ingest" do
     exhibit = FactoryGirl.create(:exhibit)
     admin = FactoryGirl.create(:tufts_exhibit_admin, exhibit: exhibit)
     sign_in(admin)
-  end
+
+    visit(spotlight.new_exhibit_resource_path(exhibit))
+
+    expect {
+      within("#new_resource") do
+        fill_in("Fedora ID:", with: pid)
+        click_button("Do It!")
+      end
+    }.to change(FedoraResource, :count).by(1)
+  end # End Ingesting a record
 
 end
 
