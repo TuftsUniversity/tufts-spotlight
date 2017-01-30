aeb669422cb1b77e0a8a6246a93d43e09c4965d2
Travis Lilleberg <travis.lilleberg@tufts.edu>
2017-01-30 11:42:48 -0500

    Fixed bug where tmp file wasn't being written too on first set of tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -84,6 +84,7 @@ def create_fedora_builder_file(file)
   obj = FedoraBuilder.new(FactoryGirl.build_stubbed(:fedora_resource), "spec/fixtures/fedora_fields.yml")
   f = File.new(file, 'w')
   f.write(obj.to_yaml)
+  f.flush
 end
 
 def clean_fedora_and_solr
