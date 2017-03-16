2a78505b4ff5cf1f54724ed93dc26f0c9453ff7b
Travis Lilleberg <travis.lilleberg@tufts.edu>
2017-03-16 16:20:38 -0400

    Fixed feature spec that broke because I removed the login link

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,7 +1,6 @@
 module LoginMacros
   def sign_in(user)
-    visit(root_path)
-    click_link('Sign in', match: :first)
+    visit('users/sign_in')
     fill_in('user_username', with: user.username)
     fill_in('user_password', with: user.password)
     click_button('Log in')
