f29881b84fa8d96bf202aa378527544a88899947
Travis Lilleberg <travis.lilleberg@tufts.edu>
2017-03-30 15:13:48 -0400

    Added some comments to ldap manager.

--- spec/support/ldap_manager.rb
+++ spec/support/ldap_manager.rb
@@ -1,8 +1,16 @@
 module LdapManager
+
+  ##
+  # Settings
+  #   @ldap_running {bool} Is ldap running already?
+
   def initialize
     @ldap_running = false;
   end
 
+  ##
+  # Put this at the top of a file that needs ldap.
+  # Will start the ldap server if it's not already started.
   def i_need_ldap
     unless(@ldap_running)
       @ldap_server = Ladle::Server.new(
@@ -14,6 +22,8 @@ module LdapManager
     end
   end
 
+  ##
+  # Stops ldap.
   def stop_ldap
     if(@ldap_running)
       @ldap_server.stop
