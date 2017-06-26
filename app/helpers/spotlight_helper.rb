066c812d34ec3480aed635bf92c426bf5649020b
Travis Lilleberg <travis.lilleberg@tufts.edu>
2017-06-26 11:03:46 -0400

    Added abbrev_field helper method to cut certain fields down in length when showing in search results. Specifically description.

--- app/helpers/spotlight_helper.rb
+++ app/helpers/spotlight_helper.rb
@@ -3,4 +3,14 @@
 module SpotlightHelper
   include ::BlacklightHelper
   include Spotlight::MainAppHelpers
+
+  def abbrev_field(text)
+    if(text.length <= 140)
+      return text
+    end
+
+    # Using 110 and 100 as ranges because little point in cutting a single word off.
+    last_space_within_range = text.slice(0..125).rindex(/\s/)
+    text.slice(0..last_space_within_range) + "  . . ."
+  end
 end
