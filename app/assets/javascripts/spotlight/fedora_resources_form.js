75e25de83161a654da8f5655c51d8d8fb73f0f16
Travis Lilleberg <travis.lilleberg@tufts.edu>
2017-07-18 09:04:02 -0400

    Capped the fields on fedora fields form to 16.

--- app/assets/javascripts/spotlight/fedora_resources_form.js
+++ app/assets/javascripts/spotlight/fedora_resources_form.js
@@ -2,30 +2,36 @@
 
 (function fedoraResourceInputsScope() {
 
-  var fedoraResourceInputs = function fedoraResourceInputSetup() {
-    var form = $("#new_resources_fedora"),
-      insert_here, clean_copy,
+  /**
+   * @function
+   * Captures the "Three More Fields" button and generates three more fields, up to 15.
+   *
+   * @param form {element} The form containing the inputs and button.
+   */
+  var fedoraResourceInputs = function fedoraResourceInputSetup(form) {
+    var times_run = 0,
+      button = form.find("#3-more-btn"),
+      clean_copy = form.find("input.form-control").clone(),
+      insert_here = form.find(".form-actions"),
       i;
 
-    // Ignore this if the form doesn't exist
-    if (form.length === 0) {
-      return false;
-    }
-
-    insert_here = form.find(".form-actions");
-    clean_copy = form.find("input.form-control").clone();
-
-    form.find("#3-more-btn").on("click", function add3BtnHndlr(e) {
+    button.on("click", function add3BtnHndlr(e) {
       e.preventDefault();
 
       for(i = 0; i < 4; i++) {
         insert_here.before(clean_copy.clone());
       }
-    }); //End click handler
+
+      times_run++;
+      if(times_run > 4) {
+        $(button).attr("disabled", "true");
+      }
+    }); // End click handler
   }; // fedoraResourceInputs function
 
 
   $(document).ready(function (){
-    fedoraResourceInputs();
+    var form = $("#new_resources_fedora");
+    if(form.length > 0) { fedoraResourceInputs(form); }
   });
 })();
