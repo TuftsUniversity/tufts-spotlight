7bcaac835e6626fb6e1b9e26373d57ec15a68885
Travis Lilleberg <travis.lilleberg@tufts.edu>
2017-08-04 08:04:24 -0400

    Fixed up the tests a bit

--- spec/features/feature_page_customizations_spec.rb
+++ spec/features/feature_page_customizations_spec.rb
@@ -11,7 +11,6 @@ feature "Feature Page customizations" do
     FactoryGirl.create(:tufts_exhibit_admin, exhibit: exhibit)
   end
 
-  # Visible page only exists to force Spotlight to create a menu.
   let(:visible_page) do
     FactoryGirl.create(
       :feature_page,
@@ -23,57 +22,46 @@ feature "Feature Page customizations" do
     FactoryGirl.create(
       :feature_page,
       title: "Hidden",
+      exhibit: exhibit,
+      in_menu: false
+    )
+  end
+  # Blank page just exists to force a dropdown menu.
+  let(:blank_page) do
+    FactoryGirl.create(
+      :feature_page,
+      title: "Filler",
       exhibit: exhibit
     )
   end
 
   before(:each) do
-    hidden_page
+    visible_page
     sign_in(exhibit_admin)
   end
 
-  scenario "'In menu' checkbox exists in form, and successfully sets value in db" do
-    visit(spotlight.edit_exhibit_feature_page_path(exhibit, hidden_page))
+  scenario "'In menu' checkbox exists in form, and sets value in db" do
+    visit(spotlight.edit_exhibit_feature_page_path(exhibit, visible_page))
 
-    expect(Spotlight::FeaturePage.find(hidden_page.id).in_menu).to be true
+    expect(Spotlight::FeaturePage.find(visible_page.id).in_menu).to be(true)
     expect(page).to have_field("In menu", { checked: true })
 
     uncheck("In menu")
     click_button("Save")
     click_link("Edit")
 
-    expect(Spotlight::FeaturePage.find(hidden_page.id).in_menu).to be false
+    expect(Spotlight::FeaturePage.find(visible_page.id).in_menu).to be(false)
     expect(page).to have_field("In menu", { unchecked: true })
   end
 
-  scenario "In menu checkbox adds/removes page from menus", js: true do
-    visible_page
+  scenario "In menu property displays/hides page in menus" do
+    blank_page
+    hidden_page
 
-    # Verify page starts in navigation.
-    visit(spotlight.exhibit_feature_page_path(exhibit, visible_page))
+    visit(spotlight.exhibit_feature_page_path(exhibit, blank_page))
     within("#sidebar") do
-      expect(has_content?("Visible")).to be(true)
-      expect(has_link?("Hidden", visible: :all)).to be(true)
-    end
-    within("#exhibit-navbar") do
-      expect(has_link?("Visible", visible: :all)).to be(true)
-      expect(has_link?("Hidden", visible: :all)).to be(true)
-    end
-
-    visit(spotlight.edit_exhibit_feature_page_path(exhibit, hidden_page))
-    click_link("Options")
-
-    # Hide "Hidden"
-    uncheck("In menu")
-    click_button("Save")
-    click_link("Edit")
-    click_link("Options")
-
-    # Verify page is no longer in navigation.
-    visit(spotlight.exhibit_feature_page_path(exhibit, visible_page))
-    within("#sidebar") do
-      expect(has_content?("Visible")).to be(true)
-      expect(has_link?("Hidden", visible: :all)).to be(false)
+      expect(has_link?("Visible")).to be(true)
+      expect(has_link?("Hidden")).to be(false)
     end
     within("#exhibit-navbar") do
       expect(has_link?("Visible", visible: :all)).to be(true)
