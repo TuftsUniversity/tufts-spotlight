72be4ad8382a908b45f7947c9785554b28a59226
Travis Lilleberg <travis.lilleberg@tufts.edu>
2018-06-28 00:04:08 -0400

    Added thumbnails and full_images to every search. Autocomplete wasn't pulling full page images.

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -4,6 +4,14 @@ class SearchBuilder < Blacklight::SearchBuilder
 
   include Spotlight::AccessControlsEnforcementSearchBuilder
 
+  self.default_processor_chain += [:add_non_iiif_image_field_to_searches]
+
+  # Autocomplete wasn't using the full_image field and messing up stuff.
+  # We need to always have thumbs and full images available.
+  def add_non_iiif_image_field_to_searches(solr_parameters)
+    solr_parameters["fl"] << " #{Spotlight::Engine.config.full_image_field}" unless solr_parameters["fl"].include? Spotlight::Engine.config.full_image_field.to_s
+    solr_parameters["fl"] << " #{Spotlight::Engine.config.thumbnail_field}" unless solr_parameters["fl"].include? Spotlight::Engine.config.thumbnail_field.to_s
+  end
   ##
   # @example Adding a new step to the processor chain
   #   self.default_processor_chain += [:add_custom_data_to_query]
