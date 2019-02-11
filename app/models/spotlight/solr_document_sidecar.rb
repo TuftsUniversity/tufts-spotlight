71b2c8d17f96ec8a5b3d33c0ce4d95bfc23a03da
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-02-11 13:24:14 -0500

    Fixed bug where empty custom fields were being indexed into Solr and displayed on the site.

---
+++ app/models/spotlight/solr_document_sidecar.rb
@@ -0,0 +1,19 @@
+#@file
+# Monkey patch for SolrDocumentSidecar.
+# Fixes issue where empty custom fields were displaying in search results and show view.
+# https://github.com/projectblacklight/spotlight/issues/2070
+require_dependency Spotlight::Engine.root.join('app', 'models', 'spotlight', 'solr_document_sidecar').to_s
+
+module Spotlight
+  class SolrDocumentSidecar < ActiveRecord::Base
+    def custom_fields_data_to_solr
+      data.except('configured_fields').each_with_object({}) do |(key, value), solr_hash|
+        next unless value.present? ## PATCH HERE ##
+        custom_field = custom_fields[key]
+        field_name = custom_field.solr_field if custom_field
+        field_name ||= key
+        solr_hash[field_name] = value
+      end
+    end
+  end
+end
