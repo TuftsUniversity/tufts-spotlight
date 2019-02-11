d532752036488f6210692461bae176573eba4f8d
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-02-11 13:53:35 -0500

    Fixed test for TravisCI

--- spec/lib/tufts/settings_spec.rb
+++ spec/lib/tufts/settings_spec.rb
@@ -1,16 +1,9 @@
 require 'rails_helper'
 
 describe Tufts::Settings do
-  let(:valid_urls) do
-    [
-      "https://tdl-dev-01.uit.tufts.edu/concern/images/",
-      "https://tdl-prod-01.uit.tufts.edu/concern/images/"
-    ]
-  end
-
   describe "load" do
     it "returns either the prod or dev tdl url for tdl_url" do
-      expect(valid_urls).to include(Tufts::Settings.load[:tdl_url])
+      expect(Tufts::Settings.load[:tdl_url]).to eq('https://dl.tufts.edu/concern/images/')
     end
   end
 end
