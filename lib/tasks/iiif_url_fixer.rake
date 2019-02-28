8381df15a5db583f49ea1c31c69f0357178006fb
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-02-28 15:46:59 -0500

    Finalized hostname fixing scripts

--- lib/tasks/iiif_url_fixer.rake
+++ lib/tasks/iiif_url_fixer.rake
@@ -5,52 +5,9 @@ namespace :tufts do
     Tufts::HostnameFixer.fix_all_tdl_resources
   end
 
-
   desc 'Updates iiif urls in feature and about pages to a new host.'
   task fix_page_iiif_references: :environment do
-    unless(File.file?(ftr_file))
-      puts "\nERROR: No json file of SolrDocument -> Resource ids."
-      exit
-    end
-    solr_map = JSON.parse(File.read(ftr_file))
-    count = 0
-
-    Spotlight::Page.all.each do |page|
-      puts "=============== Processing #{page.id} ================="
-
-      blocks = JSON.parse(page.content.to_s)
-      blocks['data'].each do |block|
-        next if(fpir_ignore_blocks.include?(block['type']))
-
-        if(fpir_no_items?(block))
-          puts "No items: #{block['data']['item']}"
-          next
-        end
-
-        block['data']['item'].each do |_, item|
-          if(!item['full_image_url'].nil? &&  !item['full_image_url'].empty?)
-            puts "#{item['full_image_url']} is an upload."
-            next
-          end
-          count = count + 1
-          tdl_resource = solr_map[item['id']]
-
-          byebug
-#          item['iiif_tilesource'][old_host]  = new_host
-#          item['iiif_manifest_url'][old_host]  = new_host
-#          item['iiif_canvas_id'][old_host]  = new_host
-        end
-
-        break if count >= 11
-      end
-      #page.content = blocks.to_json
-      #page.save
-    end
-  end
-
-  ##
-  # File that contains the old SolrDocument mappings.
-  def ftr_file
-    "#{Rails.root}/tmp/fix_iiif.json"
+    ActiveRecord::Base.logger.level = 1
+    Tufts::HostnameFixer.fix_all_page_references
   end
 end
--- lib/tufts/hostname_fixer.rb
+++ lib/tufts/hostname_fixer.rb
@@ -6,6 +6,7 @@ module Tufts
     ##
     # Runs fix_tdl_resources on all resources.
     def self.fix_all_tdl_resources
+      @solr_map = {}
       Tufts::TdlResource.all.each { |r| fix_tdl_resource(r) }
       save_solr_map
     end
@@ -50,10 +51,7 @@ module Tufts
 
       old_solr_doc_id = all_sidecars[0].document_id
       new_solr_doc_id = all_sidecars[1].document_id
-      solr_map[resource.id] = {
-        old_solr: old_solr_doc_id,
-        new_solr: new_solr_doc_id
-      }
+      @solr_map[old_solr_doc_id] = new_solr_doc_id
 
       puts "Changing #{old_solr_doc_id} to #{new_solr_doc_id} and deleting other Sidecar"
       all_sidecars[0].document_id = new_solr_doc_id
@@ -64,28 +62,84 @@ module Tufts
     ##
     # Goes through all the feature pages and fixes the iiif references to the new
     # SolrDocuments and new IIIF urls.
-    def self.fix_page_references
+    def self.fix_all_page_references
+      load_solr_map
+      Spotlight::Page.all.each do |page|
+        fix_page_references(page)
+      end
+    end
+
+    ##
+    # Goes through all the feature pages and fixes the iiif references to the new
+    # SolrDocuments and new IIIF urls.
+    def self.fix_page_references(page)
+      puts "=============== Processing #{page.id} ================="
+
+      blocks = JSON.parse(page.content.to_s)
+      blocks['data'].each do |block|
+        next if(ignore_blocks.include?(block['type']))
+
+        if(no_items?(block))
+          puts "No items: #{block['data']['item']}"
+          next
+        end
+
+        block['data']['item'].each do |_, item|
+          if(!item['full_image_url'].nil? &&  !item['full_image_url'].empty?)
+            puts "#{item['full_image_url']} is an upload."
+            next
+          end
+
+          if(!item['iiif_tilesource'].present?)
+            puts "#{item['id']} has no tilesource - #{item['iiif_tilesource']}"
+            next
+          end
+
+          if(item['iiif_tilesource'].include?(old_host))
+            fix_item(item, old_host)
+          elsif(item['iiif_tilesource'].include?(other_host))
+            fix_item(item, other_host)
+          else
+            puts "#{item['id']}'s tilesource doesn't match  - #{item['iiif_tilesource']}"
+            next
+          end
+        end
+      end
+      page.content = blocks.to_json
+      page.save
     end
 
     private
+
       ##
-      # The mappings of solr documents.
-      def self.solr_map
-        @solr_map ||= []
+      # Fix page item
+      #
+      # @param {hash} item
+      def self.fix_item(item, old_url)
+        begin
+          puts "Fixing #{item['id']} - #{item['iiif_tilesource']}"
+          new_solr_id = @solr_map[item['id']]
+          item['id'] = new_solr_id
+          item['iiif_tilesource'][old_url]  = new_host
+          item['iiif_manifest_url'][old_url]  = new_host
+          item['iiif_canvas_id'][old_url]  = new_host
+        rescue StandardError
+          byebug
+        end
       end
 
       ##
       # Write solr_map to a tmp file.
       def self.save_solr_map
         f = File.open(solr_map_file, 'w+')
-        f.write(solr_map.to_json)
+        f.write(@solr_map.to_json)
         f.close
       end
 
       ##
       # Read the solr_map file into a hash.
-      def self.open_solr_map
-        JSON.parse(File.read(solr_map_file))
+      def self.load_solr_map
+        @solr_map = JSON.parse(File.read(solr_map_file))
       end
 
       ##
@@ -126,6 +180,12 @@ module Tufts
       # The old hostname.
       def self.new_host
         'dl.tufts.edu'
+      end
+
+      ##
+      # Another old hostname.
+      def self.other_host
+        'dl1.tufts.edu'
       end
   end
 end
