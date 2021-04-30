d90cca522945759e231e59be5d814e78af56f666
Travis Lilleberg <travis.lilleberg@tufts.edu>
2021-04-30 10:19:17 -0400

    Wrote tests first. Fixed some style issues in existing tests

---
+++ spec/features/pdf_support_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+require 'webdrivers/chromedriver'
+include FeatureMacros
+i_need_ldap
+
+feature "PDF Support" do
+  let(:exhibit) { FactoryBot.create(:exhibit) }
+  let(:image) do
+    FactoryBot.create(
+      :featured_image,
+      image: File.open(File.join(Rails.root, 'spec', 'fixtures', 'stego.jpg'))
+    )
+  end
+  let(:image_upload) do
+    FactoryBot.create(
+      :uploaded_resource,
+      exhibit: exhibit,
+      upload: image
+    )
+  end
+  let(:pdf) do
+    FactoryBot.create(
+      :featured_image,
+      image: File.open(File.join(Rails.root, 'spec', 'fixtures', 'test.pdf'))
+    )
+  end
+  let(:pdf_upload) do
+    FactoryBot.create(
+      :uploaded_resource,
+      exhibit: exhibit,
+      upload: pdf
+    )
+  end
+
+  scenario 'PDFs should have a "View PDF" link on display' do
+    pdf_upload.reindex
+    sleep(1)
+    visit(spotlight.exhibit_solr_document_path(exhibit, pdf_upload.compound_id))
+    expect(page).to have_content("View PDF")
+  end
+
+  scenario 'Images should not have "View PDF" link on display' do
+    image_upload.reindex
+    sleep(1)
+    visit(spotlight.exhibit_solr_document_path(exhibit, image_upload.compound_id))
+    expect(page).not_to have_content("View PDF")
+  end
+end
