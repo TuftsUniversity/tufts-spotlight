7eddf3e9fc9933f7c4e3f58f96239587740e97d4
Mike Korcynski <mkorcy@gmail.com>
2023-02-08 11:45:10 -0500

    WIP - resolves disallowed class
    not sure it'll make much progress on the larger issue

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
+  config.active_record.yaml_column_permitted_classes = [Symbol]
 
   # Configure static file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
