ad3d446c29dc612d2fbe309a54ca57bef8d7fa93
Ryan Orlando <rynorlando@gmail.com>
2023-11-07 12:11:41 -0500

    fix legacy_connection_handling

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,4 +41,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Temp Understand this line more fully/ put it in dev and prod
+  config.active_record.legacy_connection_handling = false
 end
