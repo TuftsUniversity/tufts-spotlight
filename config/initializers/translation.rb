fe52f98232c05d9ba84e298ea8f9478b1e0ccfa5
Ryan Orlando <rynorlando@gmail.com>
2023-11-15 13:17:36 -0500

    temp

--- config/initializers/translation.rb
+++ config/initializers/translation.rb
@@ -2,34 +2,36 @@
 
 require 'i18n/backend/active_record'
 
-unless defined?(Translation)
-  Translation = I18n::Backend::ActiveRecord::Translation
-  Translation.include Spotlight::CustomTranslationExtension
+Translation = I18n::Backend::ActiveRecord::Translation
 
-  # Work-around for https://github.com/svenfuchs/i18n-active_record/pull/133
-  if Translation.respond_to?(:to_hash)
-    class << Translation
-      alias to_h to_hash
-      remove_method :to_hash
-    end
+if Translation.table_exists?
+  ##
+  # Sets up the new Spotlight Translation backend, backed by ActiveRecord. To
+  # turn on the ActiveRecord backend, uncomment the following lines.
 
-    I18n::Backend::ActiveRecord.define_method(:init_translations) do
-      @translations = Translation.to_h
-    end
-  end
+  # I18n.backend = I18n::Backend::ActiveRecord.new
+  I18n::Backend::ActiveRecord.include I18n::Backend::Memoize
+  # temp why does this not work
+  # Translation.include Spotlight::CustomTranslationExtension
+  I18n::Backend::Simple.include I18n::Backend::Memoize
+  I18n::Backend::Simple.include I18n::Backend::Pluralization
+
+  # I18n.backend = I18n::Backend::Chain.new(I18n.backend, I18n::Backend::Simple.new)
 end
 
-#  Temp try the. version of this file from spotlight
-# if Translation.table_exists?
-#   ##
-#   # Sets up the new Spotlight Translation backend, backed by ActiveRecord. To
-#   # turn on the ActiveRecord backend, uncomment the following lines.
-
-#   # I18n.backend = I18n::Backend::ActiveRecord.new
-#   I18n::Backend::ActiveRecord.include I18n::Backend::Memoize
+# unless defined?(Translation)
+#   Translation = I18n::Backend::ActiveRecord::Translation
 #   Translation.include Spotlight::CustomTranslationExtension
-#   I18n::Backend::Simple.include I18n::Backend::Memoize
-#   I18n::Backend::Simple.include I18n::Backend::Pluralization
 
-#   # I18n.backend = I18n::Backend::Chain.new(I18n.backend, I18n::Backend::Simple.new)
+#   # Work-around for https://github.com/svenfuchs/i18n-active_record/pull/133
+#   if Translation.respond_to?(:to_hash)
+#     class << Translation
+#       alias to_h to_hash
+#       remove_method :to_hash
+#     end
+
+#     I18n::Backend::ActiveRecord.define_method(:init_translations) do
+#       @translations = Translation.to_h
+#     end
+#   end
 # end
