0224cfce7cb614b68fb8c204c9dfdae9a3114cf7
Mike Korcynski <mkorcy@gmail.com>
2024-05-17 12:25:45 -0400

    rubocop

--- config/initializers/papertrail.rb
+++ config/initializers/papertrail.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
 # PaperTrail serializes objects to YAML, so we need to permit these classes to be deserialized
 if ActiveRecord.respond_to?(:yaml_column_permitted_classes)
-   # Rails >= 7.0
-   ActiveRecord.yaml_column_permitted_classes ||= []
-   ActiveRecord.yaml_column_permitted_classes += [Symbol,
-                                                  ActiveSupport::HashWithIndifferentAccess,
-                                                  ActiveSupport::TimeWithZone,
-                                                  ActiveSupport::TimeZone,
-                                                  Time]
+  # Rails >= 7.0
+  ActiveRecord.yaml_column_permitted_classes ||= []
+  ActiveRecord.yaml_column_permitted_classes += [Symbol,
+                                                 ActiveSupport::HashWithIndifferentAccess,
+                                                 ActiveSupport::TimeWithZone,
+                                                 ActiveSupport::TimeZone,
+                                                 Time]
 
 elsif ActiveRecord::Base.respond_to?(:yaml_column_permitted_classes)
   # Rails 6.1
