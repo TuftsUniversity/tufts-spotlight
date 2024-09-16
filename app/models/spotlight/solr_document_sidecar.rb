75e77558f0913299c9cde6954c70d22ca374a614
Ryan Orlando <rynorlando@gmail.com>
2024-09-16 15:28:06 -0400

    bring sidecar patch back.

---
+++ app/models/spotlight/solr_document_sidecar.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# @file
+# Monkey patch for SolrDocumentSidecar.
+# Fixes issue where empty custom fields were displaying in search results and show view.
+# https://github.com/projectblacklight/spotlight/issues/2070
+require_dependency Spotlight::Engine.root.join('app', 'models', 'spotlight', 'solr_document_sidecar').to_s
+
+module Spotlight
+  class SolrDocumentSidecar < ActiveRecord::Base
+    def custom_fields_data_to_solr
+      data.except('configured_fields').each_with_object({}) do |(key, value), solr_hash|
+        next if value.blank? ## PATCH HERE ##
+
+        custom_field = custom_fields[key]
+        field_name = custom_field.solr_field if custom_field
+        field_name ||= key
+        solr_hash[field_name] = convert_stored_value_to_solr(value)
+      end
+    end
+  end
+end
